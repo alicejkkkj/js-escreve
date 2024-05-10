091b3a8143fc1fda2f88007b1a08b964310bda6d
alicejkkkj <00001106426411SP@al.educacao.sp.gov.br>
2024-05-10 09:06:54 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+    createCanvas(800, 400);
+    background("lightgrey")
+  }
+  
+  function draw() {
+    
+  
+    
+    stroke("black") ;
+    fill("purple");
+    
+    if(mouseIsPressed)
+   //rect(mouseX,mouseY,10,20);
+    circle(mouseX,mouseY,30,20)
+    
+    
+    
+    
+  }
